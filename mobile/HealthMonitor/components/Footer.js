3179ab1a73c60f8e3ee19cc600b5990cfdfd9ed0
elthon-migotto <elthon.migotto@specchio.info>
2024-10-21 13:19:52 -0300

    first commit mobile app

---
+++ mobile/HealthMonitor/components/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Footer = () => {
+  return (
+    <View style={styles.footer}>
+      <TouchableOpacity onPress={() => alert("Home")}>
+        <Icon name="home" size={25} color={"#000"} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => alert("Calculadora")}>
+        <Icon name="calculator" size={25} color={"#000"} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    position: "absolute",
+    bottom: 0,
+    padding: 10,
+    height: '80px',
+    width: "100%",
+  },
+});
+
+export default Footer;
