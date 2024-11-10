eb0897a03471a92f5e32faafa667d3a27321b4c8
EduPZz <eduardo.pozzogood@hotmail.com>
2024-11-09 23:43:28 -0300

    [WEB] Fix smartwatch and create loggout button

--- web/health-monitor/src/layout/index.js
+++ web/health-monitor/src/layout/index.js
@@ -2,9 +2,18 @@ import React from "react";
 import "./styles.css";
 import { FaHouseChimney, FaCalendarDay, FaGear } from "react-icons/fa6";
 import logo from "../assets/Frame.png";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { FaSignOutAlt } from "react-icons/fa";
 
 function Layout() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
   return (
     <div className="appContainer">
       <div className="sidebar">
@@ -21,6 +30,9 @@ function Layout() {
           <div className="icon">
             <FaGear style={{ color: "#707070" }} />
           </div>
+        </div>
+        <div className="logoutIcon" onClick={handleLogout}>
+          <FaSignOutAlt style={{ color: "#fff", cursor: "pointer" }} />
         </div>
       </div>
       <Outlet />
--- web/health-monitor/src/pages/dashboard/dashboard.js
+++ web/health-monitor/src/pages/dashboard/dashboard.js
@@ -6,199 +6,206 @@ import { FaChevronRight } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import api from "../../services/api";
 import {
-    ResponsiveContainer,
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
 } from "recharts";
 
 function Dashboard() {
-    const dataExercicios = [
-        {
-            name: 'Segunda',
-            cardio: 30,
-            judo: 80,
-            musculacao: 80,
-        },
-        {
-            name: 'Terça',
-            cardio: 30,
-            judo: 60,
-            musculacao: 60,
-        },
-        {
-            name: 'Quarta',
-            cardio: 30,
-            judo: 60,
-            musculacao: 60,
-        },
-        {
-            name: 'Quinta',
-            cardio: 30,
-            judo: 60,
-            musculacao: 60,
-        },
-        {
-            name: 'Sexta',
-            cardio: 30,
-            judo: 60,
-            musculacao: 60,
-        },
-        {
-            name: 'Sábado',
-            cardio: 30,
-            judo: 60,
-            musculacao: 60,
-        },
-        {
-            name: 'Domingo',
-            cardio: 30,
-            judo: 60,
-            musculacao: 60,
-        }
-    ];
+  const dataExercicios = [
+    {
+      name: "Segunda",
+      cardio: 30,
+      judo: 80,
+      musculacao: 80,
+    },
+    {
+      name: "Terça",
+      cardio: 30,
+      judo: 60,
+      musculacao: 60,
+    },
+    {
+      name: "Quarta",
+      cardio: 30,
+      judo: 60,
+      musculacao: 60,
+    },
+    {
+      name: "Quinta",
+      cardio: 30,
+      judo: 60,
+      musculacao: 60,
+    },
+    {
+      name: "Sexta",
+      cardio: 30,
+      judo: 60,
+      musculacao: 60,
+    },
+    {
+      name: "Sábado",
+      cardio: 30,
+      judo: 60,
+      musculacao: 60,
+    },
+    {
+      name: "Domingo",
+      cardio: 30,
+      judo: 60,
+      musculacao: 60,
+    },
+  ];
 
-    const [dados, setDados] = useState([]);
+  const [dados, setDados] = useState([]);
 
-    useEffect(() => {
-        const fetchDados = async () => {
-            try {
-                const { data } = await api.get("/body-measure/");
-                setDados(data);
-                console.log("dentro do fetchdados", data);
-            } catch (error) {
-                console.log("erro dentro do catch", error.message);
-            }
-        };
-
-        fetchDados();
-    }, []);
-
-    console.log("Dados", dados);
-
-    const [watchCode, setWatchCode] = useState("");
-    const [smartwatch, setSmartwatch] = useState({});
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchWatchCode = async () => {
-            try {
-                const {
-                    data: { smartwatchCode },
-                } = await api.get("auth/profile");
-                setWatchCode(smartwatchCode ?? "");
-
-                if (smartwatchCode) handlePairing(smartwatchCode ?? "", true);
-            } catch (error) {
-                console.error("Erro ao buscar o código do smartwatch:", error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchWatchCode();
-    }, []);
-
-    useEffect(() => {
-        let interval;
-
-        if (smartwatch && watchCode) {
-            interval = setInterval(() => {
-                handlePairing(watchCode, false);
-            }, 5000);
-        }
-
-        return () => {
-            if (interval) clearInterval(interval);
-        };
-    }, [smartwatch, watchCode]);
-
-    const handlePairing = async (watchCode, shouldShowLoading) => {
-        try {
-            if (shouldShowLoading) setLoading(true);
-            const { data } = await api.get(`smartwatch/${watchCode}`);
-            setSmartwatch(data);
-        } catch (error) {
-            console.error("Erro", "Não foi possível sincronizar o código");
-        } finally {
-            setLoading(false);
-        }
+  useEffect(() => {
+    const fetchDados = async () => {
+      try {
+        const { data } = await api.get("/body-measure/");
+        setDados(data);
+        console.log("dentro do fetchdados", data);
+      } catch (error) {
+        console.log("erro dentro do catch", error.message);
+      }
     };
 
-    const [valueInputWatchCode, setValueInputWatchCode] = useState('');
-    const [haveCode, setHaveCode] = useState(false);
-    const handleClickWatchCode = () => {
-        setHaveCode(true)
+    fetchDados();
+  }, []);
+
+  console.log("Dados", dados);
+
+  const [watchCode, setWatchCode] = useState("");
+  const [smartwatch, setSmartwatch] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchWatchCode = async () => {
+      try {
+        const {
+          data: { smartwatchCode },
+        } = await api.get("auth/profile");
+        setWatchCode(smartwatchCode ?? "");
+
+        if (smartwatchCode) {
+          setHaveCode(true);
+          handlePairing(smartwatchCode ?? "", true);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar o código do smartwatch:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWatchCode();
+  }, []);
+
+  useEffect(() => {
+    let interval;
+
+    if (smartwatch && watchCode) {
+      interval = setInterval(() => {
+        handlePairing(watchCode, false);
+      }, 5000);
     }
 
-    return (
-        <div className="container">
-            <div className="content">
-                <div className="divWelcome">
-                    <p className="title">Resumo de sua saúde</p>
-                    <p className="subTitle">
-                        <CurrentDate />
-                    </p>
-                </div>
-                {haveCode ? (
-                   smartwatch && <SmartwatchCard smartwatch={smartwatch} />
-                ) : (
-                    <div className="divInputWatchcode">
-                        <input
-                            type="number"
-                            className="inputWatchCode"
-                            placeholder="Insira o código do smartwatch"
-                            value={valueInputWatchCode}
-                            onChange={(e) => setValueInputWatchCode(e.target.value)}
-                        />
-                        <FaChevronRight style={{ fontSize: 20 }} onClick={handleClickWatchCode} />
-                    </div>
-                )}
-                <div className="divEvolucaoExe">
-                    <header className="headerEvolucao">
-                        <p className="titleEvolucao">Evolução de exercícios</p>
-                        <select className="selectEvolucao">
-                            <option>Jan 2024</option>
-                        </select>
-                    </header>
-                    <ResponsiveContainer width="100%" height="75%">
-                        <BarChart
-                            width={500}
-                            height={300}
-                            data={dataExercicios}
-                            margin={{
-                                top: 20,
-                                right: 30,
-                                left: 20,
-                                bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Bar dataKey="judo" stackId="a" fill="#8884d8" />
-                            <Bar dataKey="cardio" stackId="a" fill="#82ca9d" />
-                            <Bar dataKey="musculacao" fill="#ffc658" />
-                        </BarChart>
-                    </ResponsiveContainer>
-                </div>
-                <div className="divProxConsultas">
-                    <p className="titleProxConsultas">Próximas consultas</p>
-                    <div className="divInfosProxConsultas">
-                        <p className="dataProxConsultas">01 de outubro de 2024</p>
-                        <p className="docProxConsultas">Consulta com Dr.Fulano</p>
-                    </div>
-                </div>
-            </div>
-            <div className="divMetricas"></div>
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [smartwatch, watchCode]);
+
+  const handlePairing = async (watchCode, shouldShowLoading) => {
+    try {
+      if (shouldShowLoading) setLoading(true);
+      const { data } = await api.get(`smartwatch/${watchCode}`);
+      setSmartwatch(data);
+      setWatchCode(watchCode);
+    } catch (error) {
+      console.error("Erro", "Não foi possível sincronizar o código");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const [valueInputWatchCode, setValueInputWatchCode] = useState("");
+  const [haveCode, setHaveCode] = useState(false);
+
+  const handleClickWatchCode = async () => {
+    handlePairing(valueInputWatchCode, true);
+    setHaveCode(true);
+  };
+
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="divWelcome">
+          <p className="title">Resumo de sua saúde</p>
+          <p className="subTitle">
+            <CurrentDate />
+          </p>
         </div>
-    );
+
+        <div className="divInputWatchcode">
+          <input
+            type="number"
+            className="inputWatchCode"
+            placeholder="Insira o código do smartwatch"
+            value={valueInputWatchCode}
+            onChange={(e) => setValueInputWatchCode(e.target.value)}
+          />
+          <FaChevronRight
+            style={{ fontSize: 20 }}
+            onClick={handleClickWatchCode}
+          />
+        </div>
+        {haveCode && smartwatch && <SmartwatchCard smartwatch={smartwatch} />}
+        <div className="divEvolucaoExe">
+          <header className="headerEvolucao">
+            <p className="titleEvolucao">Evolução de exercícios</p>
+            <select className="selectEvolucao">
+              <option>Jan 2024</option>
+            </select>
+          </header>
+          <ResponsiveContainer width="100%" height="75%">
+            <BarChart
+              width={500}
+              height={300}
+              data={dataExercicios}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="judo" stackId="a" fill="#8884d8" />
+              <Bar dataKey="cardio" stackId="a" fill="#82ca9d" />
+              <Bar dataKey="musculacao" fill="#ffc658" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="divProxConsultas">
+          <p className="titleProxConsultas">Próximas consultas</p>
+          <div className="divInfosProxConsultas">
+            <p className="dataProxConsultas">01 de outubro de 2024</p>
+            <p className="docProxConsultas">Consulta com Dr.Fulano</p>
+          </div>
+        </div>
+      </div>
+      <div className="divMetricas"></div>
+    </div>
+  );
 }
 
 export default Dashboard;
